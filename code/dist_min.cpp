9f2bb336c5496fb9688c5896107efa0e8b27c7a8
muschellij2 <muschellij2@gmail.com>
2019-10-08 17:30:53 -0400

    added some cpp

--- code/dist_min.cpp
+++ code/dist_min.cpp
@@ -1,12 +1,18 @@
 // header files to include
 #include <Rcpp.h>
+// [[Rcpp::depends(RcppProgress)]]
+#include <progress.hpp>
+#include <progress_bar.hpp>
+// [[Rcpp::export]]
 // use Rcpp namespace to avoid Rcpp::<...> repetition
 using namespace Rcpp;
 // compute and return minimum distance along with name
 // [[Rcpp::export]]
 NumericVector dist_min_rcpp(
     NumericMatrix img,
-    NumericMatrix surface) {
+    NumericMatrix surface, 
+    bool display_progress=true) {
+  
   
   // init output matrix (x X 3)
   int n = img.ncol();
@@ -14,9 +20,14 @@ NumericVector dist_min_rcpp(
   // test if rows are equal
   NumericVector minvec_value(n, NA_REAL);
   NumericVector tmp(m, NA_REAL);
+
+  Progress p(n, display_progress);
   
   // loop through each set of starting points
+  if (Progress::check_abort() )
+    return -1.0;
   for(int i = 0; i < n; i++) {
+    p.increment(); 
     NumericVector x = img( _, i );
     for(int j = 0; j < m; j++) {
       NumericVector y = surface( _, j );
