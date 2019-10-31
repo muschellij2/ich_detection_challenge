b0fcc6ddfbebef58b32fe87ae79185c32f023b87
muschellij2 <muschellij2@gmail.com>
2019-10-31 17:39:00 -0400

    updated cnn.sh

--- code/cnn.sh
+++ code/cnn.sh
@@ -2,4 +2,4 @@ module load conda;
 source activate /jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/tensorflow-gpu;
 nvidia-smi;
 export CUDA_VISIBLE_DEVICES=1;
-R --no-save cnn_128.R
+R --no-save < cnn_128.R
