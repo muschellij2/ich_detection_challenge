c40b8de8a88835eb37b53ca838fe1a33f34157e1
muschellij2 <muschellij2@gmail.com>
2019-10-30 23:54:48 -0400

    updated duplicateddata issues

--- code/cnn_128.R
+++ code/cnn_128.R
@@ -9,10 +9,16 @@ df = df %>%
   mutate(tiff = file.path(
     "tiff_128", 
     sub(".dcm", ".tiff", basename(file))))
+
+outcomes = c("any", "epidural", "intraparenchymal", "intraventricular", 
+             "subarachnoid", "subdural")
+
 train = df %>% 
   arrange(scan_id, instance_number) %>% 
   filter(group == "train") %>% 
   filter(any > 0) 
+rm(df)
+
 
 tiffs = train %>% 
   pull(tiff)
@@ -20,9 +26,18 @@ ioutcome = c("epidural")
 y = train %>% 
   select(one_of(ioutcome)) %>% 
   pull()
+train = train %>% 
+  select(scan_id, tiff, one_of(outcomes))
 read_img = function(x) {
   img = EBImage::readImage(x)
   c(imageData(img))
 }
-mat = matrix(NA, nrow = nrow(train), ncol = 128*128)
-all_data = pbapply::pbsapply()
+mat = matrix(NA_real_, nrow = nrow(train), ncol = 128*128)
+for (irow in 1:nrow(train)) {
+  print(irow)
+  img = read_img(tiffs[irow])
+  mat[irow, ] = img
+}
+
+readr::write_rds(mat, 
+                 path = file.path("predictions", "cnn_128_data.rds"))
