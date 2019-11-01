8a2370443d2d1011a5d9ccc4c1fbc017b8cf1b86
muschellij2 <muschellij2@gmail.com>
2019-10-31 22:31:13 -0400

    updated withotu_model

--- code/cnn.sh
+++ code/cnn.sh
@@ -2,4 +2,5 @@ module load conda;
 source activate /jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/tensorflow-gpu;
 nvidia-smi;
 export CUDA_VISIBLE_DEVICES=1;
+export HDF5_USE_FILE_LOCKING=FALSE ;
 R --no-save < cnn_128.R
