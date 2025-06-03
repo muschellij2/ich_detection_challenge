c44063e7acb28afe5b3db243f7cd03bd7bbd6b52
muschellij2 <muschellij2@gmail.com>
2025-06-03 14:19:25 -0400

    added pipeline and gitignore

--- R/pipeline.sh
+++ R/pipeline.sh
@@ -1,7 +1,7 @@
 Rnosave R/02_list_files.R -J FILES --mem=8G -o %x_%A.out -e %x_%A.err
 
 
-Rnosave R/03_dump_header.R -J HEADER --mem=8G -o %x_%A.out -e %x_%A.err
+Rnosave R/03_dump_header.R -J HEADER --array=1-5 --mem=8G -o %x_%A_%a.out -e %x_%A_%a.err
 
 
 # Rnosave R/03_dump_header.R -J HEADER --array=1-5109 --mem=8G -o %x_%A_%a.out -e %x_%A_%a.err
