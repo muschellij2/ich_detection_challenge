7f884f494c178dfcd39576b3c10e1f32449eb63d
muschellij2 <muschellij2@gmail.com>
2025-06-05 23:49:03 -0400

    added utils

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,23 @@ get_fold = function(default = 1L) {
   }
   ifold
 }
+
+
+remove_brackets = function(x) {
+  x = sub("^\\[", "", x)
+  x = sub("\\]$", "", x)
+}
+
+remove_forward_slash = function(x) {
+  x = sub("^/", "", x)
+  x = sub("/$", "", x)
+}
+
+remove_back_slash = function(x) {
+  x = sub("^\\\\", "", x)
+  x = sub("\\\\$", "", x)
+}
+
+gsub_under = function(x, replacement = "-") {
+  gsub(x, pattern = "_", replacement = replacement)
+}
