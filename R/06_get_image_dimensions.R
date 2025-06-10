ce54a2fde55ff38781147cbfb5b6fe6dd6b7bec4
muschellij2 <muschellij2@gmail.com>
2025-06-10 11:11:29 -0400

    getting image dimeinsions

--- R/06_get_image_dimensions.R
+++ R/06_get_image_dimensions.R
@@ -18,16 +18,16 @@ df = df %>%
 
 readr::write_rds(df, here::here("data", "nifti_image_dimensions.rds"))
 
-df = readr::read_rds(here::here("data", "nifti_image_dimensions.rds"))
-df512 = df %>%
-  filter(dim1 == 512, dim2 == 512)
-
-df512 = df512 %>%
-  mutate(
-    ctbet = here::here(
-      "CT_BET/results_folder/unet_CT_SS_202564_16627/predictions",
-      basename(file_nifti)
-    ),
-    fe = file.exists(ctbet)
-  )
+# df = readr::read_rds(here::here("data", "nifti_image_dimensions.rds"))
+# df512 = df %>%
+#   filter(dim1 == 512, dim2 == 512)
+# 
+# df512 = df512 %>%
+#   mutate(
+#     ctbet = here::here(
+#       "CT_BET/results_folder/unet_CT_SS_202564_16627/predictions",
+#       basename(file_nifti)
+#     ),
+#     fe = file.exists(ctbet)
+#   )
 
