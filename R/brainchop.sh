d3f73385e29aa37bd6796378ea28c839968167d0
muschellij2 <muschellij2@gmail.com>
2025-06-19 17:04:59 -0400

    added brainchop

--- R/brainchop.sh
+++ R/brainchop.sh
@@ -24,7 +24,7 @@ module load conda
 conda activate brainchop_env
 cd $detect
 
-files=`ls data/nifti/ID-c*.nii.gz data/nifti/ID-d*.nii.gz`
+files=`ls data/nifti/ID-0*.nii.gz data/nifti/ID-1*.nii.gz`
 ifile=${files}
 brain_outdir="data/brain_extracted_brainchop"
 mkdir -p ${brain_outdir}
