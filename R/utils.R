8c78b36ef80f36fcfb47dc3956d8e10e4e020bed
muschellij2 <muschellij2@gmail.com>
2025-06-24 11:42:45 -0400

    added utils

--- R/utils.R
+++ R/utils.R
@@ -85,3 +85,30 @@ copy_qform = function(file_nifti) {
   img_run = fslr::fslorient(tfile, opts = "-copyqform2sform")
   return(tfile)
 }
+
+
+estimate_interlaced_scan = function(x) {
+  ImagePositionPatient = NULL
+  rm(list = c("ImagePositionPatient"))
+  x = x %>% 
+    dplyr::mutate(
+      z = gsub("\\[|\\]", "", ImagePositionPatient), 
+      z = sapply(strsplit(z, "\\\\"), dplyr::nth, 3),
+      z = as.numeric(z)
+    ) %>% 
+    dplyr::arrange(z)
+  diffs = diff(x$z)
+  tab = sort(table(diffs), decreasing = TRUE)
+  expected_diff = as.numeric(names(tab)[1])
+  expected_z = x$z[1] + (0:(nrow(x)-1))*expected_diff
+  mat = abs(outer(expected_z, x$z, FUN = "-"))
+  mins = apply(mat, 2, min)
+  keep = mins < median(mins) | mins < 0.01
+  x$keep = keep
+  x
+}
+
+any_interlaced = function(x) {
+  x = estimate_interlaced_scan(x)
+  !all(x$keep)
+}
